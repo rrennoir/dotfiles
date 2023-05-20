44c57aea75c8bf868e94e226c6bdb3325135e4e0
Ryan Rennoir <ryanrennoir9@gmail.com>
2023-05-20 19:50:38 +0200

    Add neovim config

---
+++ nvim/after/plugin/catppuccin.lua
@@ -0,0 +1,10 @@
+require("catppuccin").setup({
+	transparent_background = true,
+    color_overrides = {
+        macchiato = {
+            surface1 = "#F5A97F"
+        }
+    },
+})
+ 
+vim.cmd.colorscheme "catppuccin-macchiato"
