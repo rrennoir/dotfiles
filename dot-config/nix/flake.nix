5fc7c5c09c901132ad1dc767223781be28db8c40
Ryan Rennoir <ryanrennoir9@gmail.com>
2025-06-19 13:16:03 +0200

    Fix config for touchID sudo

--- dot-config/nix/flake.nix
+++ dot-config/nix/flake.nix
@@ -109,7 +109,7 @@
         };
       };
 
-      security.pam.enableSudoTouchIdAuth = true;
+      security.pam.services.sudo_local.touchIdAuth = true;
 
       # Set Git commit hash for darwin-version.
       system = {
